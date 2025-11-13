33e1eef97ea255dad84bfcbd65b8b1ac59eeeba5
sulaimon <tsulaimon96@gmail.com>
2025-11-13 22:22:33 +0100

    CI: Frontend project Initialization

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { QueryProvider } from '@/providers/query-provider';
+
+function App() {
+    return (
+        <QueryProvider>
+            dlkdldk
+
+        </QueryProvider>
+    );
+}
+
+export default App;
